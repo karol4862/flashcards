da17d3ae3cb3b034eee76c171f37cb42f6955ffb
Karol Makara <karol_makara17@wp.pl>
2019-05-05 21:16:57 +0200

    fix react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route} from 'react-router-dom';
 import './App.scss';
 import Main from "./Main";
 import Flashcards from "./Flashcards";
