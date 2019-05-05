d7cf4e25bb0c6c073cc6a315057f8b29c2af97fa
Karol Makara <karol_makara17@wp.pl>
2019-05-05 13:55:00 +0200

    Updates

---
+++ precache-manifest.bcad0f1124e75853bdf6c069e0e54e11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ca68c3948db9d381b07735b166c8dd",
+    "url": "/flashcards/index.html"
+  },
+  {
+    "revision": "def7235ce8b786935b9a",
+    "url": "/flashcards/static/css/main.28750038.chunk.css"
+  },
+  {
+    "revision": "91b0006f794e60eb0c70",
+    "url": "/flashcards/static/js/2.910563d3.chunk.js"
+  },
+  {
+    "revision": "def7235ce8b786935b9a",
+    "url": "/flashcards/static/js/main.e133184e.chunk.js"
+  },
+  {
+    "revision": "3afa89cd04d892312605",
+    "url": "/flashcards/static/js/runtime~main.4a72e263.js"
+  }
+]);
